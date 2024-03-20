4e36cd57cea6fcd53e1c1b55dad226aa84367185
EtiennePrimeau <etienne412@hotmail.com>
2024-03-20 14:30:12 -0400

    Gun shoots at closest enemy

--- Assets/Scripts/Etienne/Weapon.cs
+++ Assets/Scripts/Etienne/Weapon.cs
@@ -29,10 +29,35 @@ namespace SpaceBaboon
 
         private void Attack()
         {
-            Vector2 direction = transform.position - transform.root.position;
+            //Vector2 direction = transform.position - transform.root.position;
+            Vector2 direction = AimAtClosestEnemy();
             var projectile = Instantiate(m_weaponData.projectilePrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
 
             projectile.GetComponent<Projectile>()?.Shoot(direction);
+        }
+
+        private Vector2 AimAtClosestEnemy()
+        {
+
+            for (int i = 0; i < m_weaponData.maxRange; i++)
+            {
+                var colliders = Physics2D.OverlapCircleAll(transform.position, i);
+
+                foreach (var collider in colliders)
+                {
+                    if (collider.gameObject.tag == "Enemy")
+                    {
+                        Vector2 enemyPosition = collider.gameObject.transform.position;
+                        Vector2 enemyDirection = enemyPosition - new Vector2(transform.position.x, transform.position.y);
+                        return enemyDirection;
+                    }
+                }
+                
+
+            }
+
+            //Didn't find an enemy
+            return Vector2.up;
         }
     }
 }
