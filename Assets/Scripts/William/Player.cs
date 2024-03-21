bcba24b7602539d2abc4be89c1c8b489193e1db7
thebarpon <poroy995@gmail.com>
2024-03-20 21:05:13 -0400

    *Moved logic from playerCraftingInteraction to Player -Removed playerCraftingInteraction

--- Assets/Scripts/William/Player.cs
+++ Assets/Scripts/William/Player.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,20 +7,31 @@ namespace SpaceBaboon
     {
         [SerializeField]
         private PlayerData m_playerData;
-        private Dictionary<SpaceBaboon.InteractableResource.EResourceType,int> m_collectibleInventory;
+        [SerializeField]
+        private bool m_DebugMode = false;
+        private Dictionary<SpaceBaboon.InteractableResource.EResourceType, int> m_collectibleInventory;
         private List<Weapon> m_equipedWeapon;
         private List<Weapon> m_blockedWeapon;
 
         // Start is called before the first frame update
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (m_DebugMode && collision.gameObject.tag == "Structure") { Debug.Log("CollisionDetected with structure"); }
+
+            if (collision.gameObject.tag == "Structure")
+            {
+                collision.GetComponent<ResourceDropPoint>().CollectResource();
+            }
         }
     }
 }
