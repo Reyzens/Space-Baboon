bc56710257eafb5db34c9d6a0628eaac221a9296
thebarpon <poroy995@gmail.com>
2024-03-22 14:35:13 -0400

    *Add resource from player to crafting station

--- Assets/Scripts/P-O/CraftingStation.cs
+++ Assets/Scripts/P-O/CraftingStation.cs
@@ -14,12 +14,14 @@ namespace SpaceBaboon
         private List<ResourceDropPoint> resourceDropPoints = new List<ResourceDropPoint>();
         [SerializeField]
         private float levelScaling;
+        [SerializeField]
+        private bool m_DebugMode;
 
         //Private variables
         private Transform m_position;
         private float currentHealth;
-        private List<ResourceData> resourceNeeded = new List<ResourceData>();
-        private List<InteractableResource> currentResources = new List<InteractableResource>();
+        private List<SpaceBaboon.InteractableResource.EResourceType> resourceNeeded = new List<SpaceBaboon.InteractableResource.EResourceType>();
+        private List<SpaceBaboon.InteractableResource.EResourceType> currentResources = new List<SpaceBaboon.InteractableResource.EResourceType>();
 
         //Static variables
         //static Upgrade currentUpgrade;
@@ -30,7 +32,7 @@ namespace SpaceBaboon
         // Start is called before the first frame update
         void Start()
         {
-
+            TemporaryInitialization();
         }
 
         // Update is called once per frame
@@ -45,6 +47,27 @@ namespace SpaceBaboon
             {
                 resourceDropPoint.AllocateResource(SpaceBaboon.InteractableResource.EResourceType.Two, 3);
             }
+        }
+
+        public void AddResource(SpaceBaboon.InteractableResource.EResourceType resourceType)
+        {
+            currentResources.Add(resourceType);
+            currentResources.Sort();
+            if (m_DebugMode)
+            {
+                Debug.Log("AddResource called on " + gameObject.name);
+                foreach (SpaceBaboon.InteractableResource.EResourceType resource in currentResources)
+                {
+                    Debug.Log("For crafting station " + gameObject.name + " there is a " + resource);
+                }
+            }
+
+            CheckIfUpgradable();
+        }
+
+        private void CheckIfUpgradable()
+        {
+
         }
     }
 }
--- Assets/Scripts/P-O/ResourceDropPoint.cs
+++ Assets/Scripts/P-O/ResourceDropPoint.cs
@@ -7,6 +7,8 @@ namespace SpaceBaboon
         //Serialized variables
         [SerializeField]
         private bool m_DebugMode = false;
+        [SerializeField]
+        private CraftingStation m_craftingStation;
 
         //Private variables
         private int m_resourceAmountNeeded;
@@ -28,11 +30,16 @@ namespace SpaceBaboon
         {
             if (m_DebugMode) { Debug.Log("Player activated CollectResource on station"); }
 
-            playerRef.DropResource(m_resourceTypeNeeded, m_resourceAmountNeeded);
+            if (playerRef.DropResource(m_resourceTypeNeeded, m_resourceAmountNeeded))
+            {
+                if (m_DebugMode) { Debug.Log("Calling AddResource on " + m_craftingStation.gameObject.name); }
+                m_craftingStation.AddResource(m_resourceTypeNeeded);
+            }
         }
 
         public void AllocateResource(SpaceBaboon.InteractableResource.EResourceType resourceType, int resourceAmount)
         {
+            if (m_DebugMode) { Debug.Log("To " + gameObject.name + " was allocated " + resourceAmount + " " + resourceType); }
             m_resourceTypeNeeded = resourceType;
             m_resourceAmountNeeded = resourceAmount;
         }
