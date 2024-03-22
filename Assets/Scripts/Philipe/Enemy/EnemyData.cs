3cfee7b2494a4629e5c3c0fdee4e3d97627bd89c
philplaisir <p.montplaisir-m@hotmail.com>
2024-03-21 20:16:18 -0400

    + EnemyData scriptable object, + MeleeEnemyData, added Enemy namespace, removed redundant L in my name in project folders

---
+++ Assets/Scripts/Philipe/Enemy/EnemyData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace SpaceBaboon.Enemy
+{
+    [CreateAssetMenu(fileName = "NewEnemyData", menuName = "SpaceBaboon/ScriptableObjects/EnemyData")]
+    public class EnemyData : ScriptableObject
+    {
+        public string name;
+        public Sprite sprite;
+        public int baseHealth;
+        public int baseDamage;        
+        public float size;
+        public float baseSpeed;
+        public float baseAttackRange;
+        public float baseAttackCooldown;
+    }
+}
