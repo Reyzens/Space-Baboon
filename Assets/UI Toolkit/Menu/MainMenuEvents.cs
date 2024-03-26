76b72dd282a321b872feedf2b11a1f60d5dcbf1e
EtiennePrimeau <etienne412@hotmail.com>
2024-03-26 16:44:03 -0400

    Created Main Menu + menu uxml & uss + transitions to mainLevel

---
+++ Assets/UI Toolkit/Menu/MainMenuEvents.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace SpaceBaboon.MenuSystem
+{
+    public class MainMenuEvents : MonoBehaviour
+    {
+        private UIDocument m_uiDoc;
+
+        private Button m_startButton;
+        private Button m_settingsButton;
+        private Button m_quitButton;
+
+        [SerializeField] private Object m_lobbyScene;
+
+        private void Awake()
+        {
+            m_uiDoc = GetComponent<UIDocument>();
+            VisualElement visualElement = m_uiDoc.rootVisualElement;
+            
+            m_startButton = visualElement.Q<Button>("StartButton");
+            m_settingsButton = visualElement.Q<Button>("SettingsButton");
+            m_quitButton = visualElement.Q<Button>("QuitButton");
+
+        }
+
+        private void OnEnable()
+        {
+            m_startButton.clicked += StartGame;
+            m_settingsButton.clicked += OpenSettings;
+            m_quitButton.clicked += QuitGame;
+        }
+
+        private void OnDisable()
+        {
+            m_startButton.clicked -= StartGame;
+            m_settingsButton.clicked -= OpenSettings;
+            m_quitButton.clicked -= QuitGame;
+        }
+
+        private void StartGame()
+        {
+            //Debug.Log("Start Game");
+
+            SceneManager.LoadScene(m_lobbyScene.name);
+        }
+
+        private void OpenSettings()
+        {
+            Debug.Log("Open Settings");
+        }
+
+        private void QuitGame()
+        {
+            //Application.Quit();
+            Debug.Log("Quit Game");
+        }
+    }
+}
