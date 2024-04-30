64b8d5166fb4d20a695529599c0fc5313831a2ef
EtiennePrimeau <etienne412@hotmail.com>
2024-04-29 22:03:15 -0400

    Added stats tracked by the pool for each object //  SpawnedTotal - UnspawnedTotal - ActiveTotal

--- Assets/Scripts/Etienne/Object Pool/GenericObjectPool.cs
+++ Assets/Scripts/Etienne/Object Pool/GenericObjectPool.cs
@@ -9,7 +9,8 @@ namespace SpaceBaboon.PoolingSystem
         [SerializeField] private GameObject m_container;
         [SerializeField] private int m_poolStartingSize;
 
-        private Dictionary<string, Queue<GameObject>> m_pooledObjects = new Dictionary<string, Queue<GameObject>>();
+        private Dictionary<string, PoolElement> m_pooledObjects = new Dictionary<string, PoolElement>();
+        //private Dictionary<string, Queue<GameObject>> m_pooledObjects = new Dictionary<string, Queue<GameObject>>();
 
         public void CreatePool(List<GameObject> prefabList, string poolName)
         {
@@ -26,26 +27,35 @@ namespace SpaceBaboon.PoolingSystem
                     m_poolStartingSize = 10;
                 }
 
-                Queue<GameObject> newQueue = new Queue<GameObject>();
+                //Queue<GameObject> newQueue = new Queue<GameObject>();
+                PoolElement newPool = new PoolElement();
                 for (int i = 0; i < m_poolStartingSize; i++)
                 {
                     GameObject obj = GameObject.Instantiate(prefab, m_container.transform);
 
                     obj.GetComponent<IPoolableGeneric>().Deactivate();
-                    newQueue.Enqueue(obj);
+                    //newQueue.Enqueue(obj);
+                    newPool.queue.Enqueue(obj);
                 }
 
                 string keyName = prefab.name + "(Clone)";
-                m_pooledObjects.Add(keyName, newQueue);
+                //m_pooledObjects.Add(keyName, newQueue);
+                m_pooledObjects.Add(keyName, newPool);
             }
         }
 
         public GameObject Spawn(GameObject prefabToSpawn, Vector2 pos)
         {
             string keyName = prefabToSpawn.name + "(Clone)";
-            if (m_pooledObjects[keyName].Count != 0)
+
+            m_pooledObjects[keyName].spawnedObjectsTotal++;     //new
+            m_pooledObjects[keyName].currentActiveObjects++;    //new
+
+            //if (m_pooledObjects[keyName].Count != 0)
+            if (m_pooledObjects[keyName].queue.Count != 0)
             {
-                var obj = m_pooledObjects[keyName].Dequeue();
+                //var obj = m_pooledObjects[keyName].Dequeue();
+                var obj = m_pooledObjects[keyName].queue.Dequeue();
 
                 var pooledObj = obj.GetComponent<IPoolableGeneric>();
                 pooledObj.Activate(pos, this);
@@ -54,11 +64,6 @@ namespace SpaceBaboon.PoolingSystem
             }
 
             //If pool is empty 
-            if (m_pooledObjects[keyName].Count > 51)
-            {
-                Debug.Log("Pool is empty");
-                return null;
-            }
             GameObject newObj = GameObject.Instantiate(prefabToSpawn, m_container.transform);
             newObj.GetComponent<IPoolableGeneric>().Activate(pos, this);
             //Debug.Log("activating new : " + prefabToSpawn.name);
@@ -77,12 +82,49 @@ namespace SpaceBaboon.PoolingSystem
             pooledObject.Deactivate();
 
             //Debug.Log("unspawn : " + obj.name);
-            m_pooledObjects[obj.name].Enqueue(obj);
+            //m_pooledObjects[obj.name].Enqueue(obj);
+            m_pooledObjects[obj.name].queue.Enqueue(obj);
+
+            m_pooledObjects[obj.name].unspawnedObjectsTotal++;      //new
+            m_pooledObjects[obj.name].currentActiveObjects--;       //new
         }
 
         public void SetPoolStartingSize(int poolSize)
         {
             m_poolStartingSize = poolSize;
         }
+
+        public int GetSpawnedTotalByObject(GameObject prefab)
+        {
+            string keyName = prefab.name + "(Clone)";
+            return m_pooledObjects[keyName].spawnedObjectsTotal;
+        }
+
+        public int GetUnspawnedTotalByObject(GameObject prefab)
+        {
+            string keyName = prefab.name + "(Clone)";
+            return m_pooledObjects[keyName].unspawnedObjectsTotal;
+        }
+
+        public int GetCurrentActiveByObject(GameObject prefab)
+        {
+            string keyName = prefab.name + "(Clone)";
+            return m_pooledObjects[keyName].currentActiveObjects;
+        }
+
+
+
+
+
+    
+        private class PoolElement
+        {
+            public Queue<GameObject> queue = new Queue<GameObject>();
+            public int spawnedObjectsTotal;
+            public int unspawnedObjectsTotal;
+            public int currentActiveObjects;
+        }
+
     }
+
 }
