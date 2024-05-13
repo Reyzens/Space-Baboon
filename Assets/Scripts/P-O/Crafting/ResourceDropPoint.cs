3efcffc008dce2e21271b058e947730458367bb3
thebarpon <poroy995@gmail.com>
2024-05-13 19:32:47 -0400

    *fixed some bug with drop station +Added some bug to drop station

--- Assets/Scripts/P-O/Crafting/ResourceDropPoint.cs
+++ Assets/Scripts/P-O/Crafting/ResourceDropPoint.cs
@@ -14,6 +14,8 @@ namespace SpaceBaboon.Crafting
         private GameObject m_circleRef;
         [SerializeField]
         private GameObject m_circlePercentRef;
+        [SerializeField]
+        private GameObject m_circleMask;
 
         //Private variables
         private int m_resourceAmountNeeded;
@@ -21,7 +23,7 @@ namespace SpaceBaboon.Crafting
         private Crafting.InteractableResource.EResourceType m_resourceTypeNeeded;
         private SpriteMask m_dropPointMask;
         private SpriteRenderer m_circleDropPointref;
-        
+
 
         // Start is called before the first frame update
         void Awake()
@@ -34,15 +36,15 @@ namespace SpaceBaboon.Crafting
         // Update is called once per frame
         void Update()
         {
-           UpdateMaskSize();
+            UpdateMaskSize();
         }
 
         private void UpdateMaskSize()
         {
             m_circlePercentRef.transform.localScale = new Vector3(NewSize(), NewSize(), NewSize());
-            m_circlePercentRef.GetComponent<SpriteRenderer>().color = new Color(m_circlePercentRef.GetComponent<SpriteRenderer>().color.r, 
-                m_circlePercentRef.GetComponent<SpriteRenderer>().color.g, 
-                m_circlePercentRef.GetComponent<SpriteRenderer>().color.b, 
+            m_circlePercentRef.GetComponent<SpriteRenderer>().color = new Color(m_circlePercentRef.GetComponent<SpriteRenderer>().color.r,
+                m_circlePercentRef.GetComponent<SpriteRenderer>().color.g,
+                m_circlePercentRef.GetComponent<SpriteRenderer>().color.b,
                 0.5f);
         }
         private float NewSize()
@@ -50,19 +52,25 @@ namespace SpaceBaboon.Crafting
             float playerResources = GameManager.Instance.Player.GetResources((int)m_resourceTypeNeeded);
             float sizeRatio;
             // Ensure we don't divide by zero
-            if (playerResources == 0)
-                return 0;  // Return full size when no resources are available
+            //if (playerResources == 0)
+            //    return 0;  // Return full size when no resources are available
+
+            if (!m_craftingStation.ResourceIsNeeded(m_resourceTypeNeeded) || playerResources == 0)
+                return 0;
+
             if (playerResources == 1)
             {
                 sizeRatio = playerResources / m_resourceAmountNeeded * m_circleDropPointref.transform.localScale.x;
             }
             else
             {
-                sizeRatio = playerResources + 1.0f / m_resourceAmountNeeded * m_circleDropPointref.transform.localScale.x;
+                sizeRatio = playerResources / m_resourceAmountNeeded * m_circleDropPointref.transform.localScale.x;
             }
-
+            Debug.Log("The player have " + playerResources + " of " + m_resourceTypeNeeded);
+            Debug.Log("The player need " + m_resourceAmountNeeded + " of " + m_resourceTypeNeeded);
             // Calculate the inverse ratio to make the mask smaller as resources increase
             float newSize = Mathf.Clamp(sizeRatio, 0f, 1f);
+            newSize *= m_circleMask.transform.localScale.x;
 
             return newSize;
         }
